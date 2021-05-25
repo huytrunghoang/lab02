2184b008247133c6e1b15c0ae693a20012c53769
809611 <809611@sait228007it.ACDM.DS.SAIT.CA>
2021-05-25 11:14:06 -0600

    HelloWorld add HelloWorld Add calculator

--- src/lab2/Lab2.java
+++ src/lab2/Lab2.java
@@ -16,6 +16,9 @@ public class Lab2 {
      */
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        int a = 2;
+        int b = 3;
+        System.out.println("a + b" + (a + b));
     }
     
 }
